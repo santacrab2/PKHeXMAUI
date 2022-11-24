aa9716ba0529cd4f08a5b2f7a7f51ba8d3414a95
Santacrab2 <skaradiodj@gmail.com>
2022-11-23 22:29:49 -0500

    pk9 class fully inherits pkm now so thats good. Added some more changes. Still trying to figure out how to get location for writing files. Currently am hardcoding locations but that doesnt seem viable.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,15 +13,17 @@ public partial class MainPage : ContentPage
         specieslabel.ItemsSource = Enum.GetValues(typeof(Species));
         naturepicker.ItemsSource = Enum.GetValues(typeof(Nature));
         Teratypepicker.ItemsSource= Enum.GetValues(typeof(MoveType));
+       
         
 
     }
-
+    public PK9 pk;
+    public FileResult pkfile;
    public async void pk9picker_Clicked(object sender, EventArgs e)
     {
-        var pkfile = await FilePicker.PickAsync();
+        pkfile = await FilePicker.PickAsync();
         var pkbytes = await File.ReadAllBytesAsync(pkfile.FullPath);
-        PK9 pk = new(pkbytes);
+        pk = new(pkbytes);
        
         specieslabel.SelectedIndex = pk.Species;
         displaypid.Text = $"{pk.PID:X}";
@@ -34,6 +36,26 @@ public partial class MainPage : ContentPage
         formpicker.ItemsSource = forms;
         formpicker.SelectedIndex = pk.Form;
 
+    }
+
+    public async void pk9saver_Clicked(object sender, EventArgs e)
+    {
+        
+        await File.WriteAllBytesAsync($"/storage/emulated/0/Documents/{(Species)pk.Species}.pk9", pk.pkdata);
+    }
+
+    private void specieschanger(object sender, EventArgs e) => pk.Species = (ushort)specieslabel.SelectedIndex;
+
+    private void rollpid(object sender, EventArgs e) 
+    { 
+        pk.SetPIDGender(pk.Gender);
+        displaypid.Text = $"{pk.PID:X}";
+    }
+
+    private void applynickname(object sender, TextChangedEventArgs e)
+    {
+        pk.SetNickname(pk.Nickname);
+        nickname.Text = pk.Nickname;
     }
 }
 
