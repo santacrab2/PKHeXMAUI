e580049be041a114d060e0f93f8cf18f812696d4
Santacrab2 <skaradiodj@gmail.com>
2022-11-25 16:59:45 -0500

    made the icon pretty. Added a few components from sysbot.base, adding the entire dll did not make Maui happy. Will read a pk9, fill out the main page, and can be sent to your switch if you have CFW/sysbot.base.

---
+++ BotBaseRoutines.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Text;
+using SysBot.Base;
+
+using System.Diagnostics;
+
+
+namespace pk9reader
+{
+    public class BotBaseRoutines
+    {
+        public int MaximumTransferSize { get; set; } = 0x1C0;
+        public int BaseDelay { get; set; } = 64;
+        public int DelayFactor { get; set; } = 256;
+        private int Read(byte[] buffer)
+        {
+            int br = MainPage.SwitchConnection.Receive(buffer, 0, 1, SocketFlags.None);
+            while (buffer[br - 1] != (byte)'\n')
+                br += MainPage.SwitchConnection.Receive(buffer, br, 1, SocketFlags.None);
+            return br;
+        }
+
+        public async Task<int> SendAsync(byte[] buffer) => await Task.Run(() => MainPage.SwitchConnection.Send(buffer)).ConfigureAwait(false);
+
+        private async Task<byte[]> ReadBytesFromCmdAsync(byte[] cmd, int length)
+        {
+            await SendAsync(cmd).ConfigureAwait(false);
+
+            var buffer = new byte[(length * 2) + 1];
+            var _ = Read(buffer);
+            return SysBot.Base.Decoder.ConvertHexByteStringToBytes(buffer);
+        }
+        public async Task<byte[]> ReadBytesAsync(uint offset, int length ) => await Read(offset, length, SysBot.Base.SwitchOffsetType.Heap).ConfigureAwait(false);
+        public async Task<byte[]> ReadBytesMainAsync(ulong offset, int length) => await Read(offset, length, SysBot.Base.SwitchOffsetType.Main).ConfigureAwait(false);
+        public async Task<byte[]> ReadBytesAbsoluteAsync(ulong offset, int length) => await Read(offset, length, SwitchOffsetType.Absolute).ConfigureAwait(false);
+
+        public async Task<byte[]> ReadBytesMultiAsync(IReadOnlyDictionary<ulong, int> offsetSizes) => await ReadMulti(offsetSizes, SwitchOffsetType.Heap).ConfigureAwait(false);
+        public async Task<byte[]> ReadBytesMainMultiAsync(IReadOnlyDictionary<ulong, int> offsetSizes) => await ReadMulti(offsetSizes, SysBot.Base.SwitchOffsetType.Main).ConfigureAwait(false);
+        public async Task<byte[]> ReadBytesAbsoluteMultiAsync(IReadOnlyDictionary<ulong, int> offsetSizes) => await ReadMulti(offsetSizes, SwitchOffsetType.Absolute).ConfigureAwait(false);
+
+        public async Task WriteBytesAsync(byte[] data, uint offset) => await Write(data, offset, SwitchOffsetType.Heap).ConfigureAwait(false);
+        public async Task WriteBytesMainAsync(byte[] data, ulong offset) => await Write(data, offset, SysBot.Base.SwitchOffsetType.Main).ConfigureAwait(false);
+        public async Task WriteBytesAbsoluteAsync(byte[] data, ulong offset) => await Write(data, offset, SwitchOffsetType.Absolute).ConfigureAwait(false);
+
+        public async Task<ulong> GetMainNsoBaseAsync()
+        {
+            byte[] baseBytes = await ReadBytesFromCmdAsync(SwitchCommand.GetMainNsoBase(), sizeof(ulong)).ConfigureAwait(false);
+            Array.Reverse(baseBytes, 0, 8);
+            return BitConverter.ToUInt64(baseBytes, 0);
+        }
+
+        public async Task<ulong> GetHeapBaseAsync()
+        {
+            var baseBytes = await ReadBytesFromCmdAsync(SwitchCommand.GetHeapBase(), sizeof(ulong)).ConfigureAwait(false);
+            Array.Reverse(baseBytes, 0, 8);
+            return BitConverter.ToUInt64(baseBytes, 0);
+        }
+
+        public async Task<string> GetTitleID()
+        {
+            var bytes = await ReadRaw(SwitchCommand.GetTitleID(), 17).ConfigureAwait(false);
+            return Encoding.ASCII.GetString(bytes).Trim();
+        }
+
+        private async Task<byte[]> Read(ulong offset, int length, SwitchOffsetType type)
+        {
+            var method = type.GetReadMethod();
+            if (length <= MaximumTransferSize)
+            {
+                var cmd = method(offset, length);
+                return await ReadBytesFromCmdAsync(cmd, length).ConfigureAwait(false);
+            }
+
+            byte[] result = new byte[length];
+            for (int i = 0; i < length; i += MaximumTransferSize)
+            {
+                int len = MaximumTransferSize;
+                int delta = length - i;
+                if (delta < MaximumTransferSize)
+                    len = delta;
+
+                var cmd = method(offset + (uint)i, len);
+                var bytes = await ReadBytesFromCmdAsync(cmd, len).ConfigureAwait(false);
+                bytes.CopyTo(result, i);
+                await Task.Delay((MaximumTransferSize / DelayFactor) + BaseDelay).ConfigureAwait(false);
+            }
+            return result;
+        }
+
+        private async Task<byte[]> ReadMulti(IReadOnlyDictionary<ulong, int> offsetSizes, SwitchOffsetType type)
+        {
+            var method = type.GetReadMultiMethod();
+            var cmd = method(offsetSizes);
+            var totalSize = offsetSizes.Values.Sum();
+            return await ReadBytesFromCmdAsync(cmd, totalSize).ConfigureAwait(false);
+        }
+
+        private async Task Write(byte[] data, ulong offset, SwitchOffsetType type)
+        {
+            var method = type.GetWriteMethod();
+            if (data.Length <= MaximumTransferSize)
+            {
+                var cmd = method(offset, data);
+                await SendAsync(cmd).ConfigureAwait(false);
+                return;
+            }
+            int byteCount = data.Length;
+            for (int i = 0; i < byteCount; i += MaximumTransferSize)
+            {
+                var slice =  data.SliceSafe(i, MaximumTransferSize);
+                var cmd = method(offset + (uint)i, slice);
+                await SendAsync(cmd).ConfigureAwait(false);
+                await Task.Delay((MaximumTransferSize / DelayFactor) + BaseDelay).ConfigureAwait(false);
+            }
+        }
+
+        public async Task<byte[]> ReadRaw(byte[] command, int length)
+        {
+            await SendAsync(command).ConfigureAwait(false);
+            var buffer = new byte[length];
+            var _ = Read(buffer);
+            return buffer;
+        }
+
+        public async Task SendRaw(byte[] command)
+        {
+            await SendAsync(command).ConfigureAwait(false);
+        }
+
+        public async Task<byte[]> PointerPeek(int size, IEnumerable<long> jumps)
+        {
+            return await ReadBytesFromCmdAsync(SwitchCommand.PointerPeek(jumps, size), size).ConfigureAwait(false);
+        }
+
+        public async Task PointerPoke(byte[] data, IEnumerable<long> jumps)
+        {
+            await SendAsync(SwitchCommand.PointerPoke(jumps, data)).ConfigureAwait(false);
+        }
+
+        public async Task<ulong> PointerAll(IEnumerable<long> jumps)
+        {
+            var offsetBytes = await ReadBytesFromCmdAsync(SwitchCommand.PointerAll(jumps), sizeof(ulong)).ConfigureAwait(false);
+            Array.Reverse(offsetBytes, 0, 8);
+            return BitConverter.ToUInt64(offsetBytes, 0);
+        }
+
+        public async Task<ulong> PointerRelative(IEnumerable<long> jumps)
+        {
+            var offsetBytes = await ReadBytesFromCmdAsync(SwitchCommand.PointerRelative(jumps), sizeof(ulong)).ConfigureAwait(false);
+            Array.Reverse(offsetBytes, 0, 8);
+            return BitConverter.ToUInt64(offsetBytes, 0);
+        }
+        public async Task Click(SwitchButton b, int delay)
+        {
+            await SendAsync(SwitchCommand.Click(b, true)).ConfigureAwait(false);
+            await Task.Delay(delay).ConfigureAwait(false);
+        }
+
+        public async Task PressAndHold(SwitchButton b, int hold, int delay)
+        {
+            await SendAsync(SwitchCommand.Hold(b, true)).ConfigureAwait(false);
+            await Task.Delay(hold).ConfigureAwait(false);
+            await SendAsync(SwitchCommand.Release(b, true)).ConfigureAwait(false);
+            await Task.Delay(delay).ConfigureAwait(false);
+        }
+
+        public async Task DaisyChainCommands(int delay, IEnumerable<SwitchButton> buttons)
+        {
+            SwitchCommand.Configure(SwitchConfigureParameter.mainLoopSleepTime, delay, true);
+            var commands = buttons.Select(z => SwitchCommand.Click(z, true)).ToArray();
+            var chain = commands.SelectMany(x => x).ToArray();
+            await SendAsync(chain).ConfigureAwait(false);
+            SwitchCommand.Configure(SwitchConfigureParameter.mainLoopSleepTime, 0, true);
+        }
+
+        public async Task SetStick(SwitchStick stick, short x, short y, int delay)
+        {
+            var cmd = SwitchCommand.SetStick(stick, x, y, true);
+            await SendAsync(cmd).ConfigureAwait(false);
+            await Task.Delay(delay).ConfigureAwait(false);
+        }
+
+        public async Task DetachController()
+        {
+            await SendAsync(SwitchCommand.DetachController(true)).ConfigureAwait(false);
+        }
+
+        public async Task SetScreen(ScreenState state)
+        {
+            await SendAsync(SwitchCommand.SetScreen(state, true)).ConfigureAwait(false);
+        }
+
+        public async Task EchoCommands(bool value)
+        {
+            var cmd = SwitchCommand.Configure(SwitchConfigureParameter.echoCommands, value ? 1 : 0, true);
+            await SendAsync(cmd).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc cref="ReadUntilChanged(ulong,byte[],int,int,bool,bool,CancellationToken)"/>
+        public async Task<bool> ReadUntilChanged(uint offset, byte[] comparison, int waitms, int waitInterval, bool match) =>
+            await ReadUntilChanged(offset, comparison, waitms, waitInterval, match, false).ConfigureAwait(false);
+
+        /// <summary>
+        /// Reads an offset until it changes to either match or differ from the comparison value.
+        /// </summary>
+        /// <returns>If <see cref="match"/> is set to true, then the function returns true when the offset matches the given value.<br>Otherwise, it returns true when the offset no longer matches the given value.</br></returns>
+        public async Task<bool> ReadUntilChanged(ulong offset, byte[] comparison, int waitms, int waitInterval, bool match, bool absolute)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            do
+            {
+                var task = absolute
+                    ? ReadBytesAbsoluteAsync(offset, comparison.Length)
+                    : ReadBytesAsync((uint)offset, comparison.Length);
+                var result = await task.ConfigureAwait(false);
+                if (match == result.SequenceEqual(comparison))
+                    return true;
+
+                await Task.Delay(waitInterval).ConfigureAwait(false);
+            } while (sw.ElapsedMilliseconds < waitms);
+            return false;
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using PKHeX.Core;
+﻿
+using PKHeX.Core;
+using System.Net.Sockets;
+
 
 
 namespace pk9reader;
@@ -13,36 +16,73 @@ public partial class MainPage : ContentPage
         specieslabel.ItemsSource = Enum.GetValues(typeof(Species));
         naturepicker.ItemsSource = Enum.GetValues(typeof(Nature));
         Teratypepicker.ItemsSource= Enum.GetValues(typeof(MoveType));
+        MainTeratypepicker.ItemsSource = Enum.GetValues(typeof(MoveType));
+        naturepicker.ItemsSource = Enum.GetValues(typeof(Nature));
+        helditempicker.ItemsSource = Util.GetItemsList("en");
        
-        
+
+
+
+
 
     }
-    public PK9 pk;
-    public FileResult pkfile;
-   public async void pk9picker_Clicked(object sender, EventArgs e)
+    BotBaseRoutines botBase = new();
+    PK9 pk = new();
+    public static Socket SwitchConnection = new Socket(SocketType.Stream, ProtocolType.Tcp);
+    public async void pk9picker_Clicked(object sender, EventArgs e)
     {
-        pkfile = await FilePicker.PickAsync();
-        var pkbytes = await File.ReadAllBytesAsync(pkfile.FullPath);
-        pk = new(pkbytes);
-       
+        
+        var pkfile = await FilePicker.PickAsync();
+        var bytes= File.ReadAllBytes(pkfile.FullPath);
+        pk = new(bytes);
+
+        if (pk.IsShiny)
+            shinybutton.Text = "★";
         specieslabel.SelectedIndex = pk.Species;
         displaypid.Text = $"{pk.PID:X}";
         nickname.Text = pk.Nickname;
         exp.Text = $"{pk.EXP}";
+        leveldisplay.Text = $"{Experience.GetLevel(pk.EXP, pk.PersonalInfo.EXPGrowth)}";
         naturepicker.SelectedIndex = pk.Nature;
-        Teratypepicker.SelectedIndex = pk.TeraType == 0x13 ? pk.MainType : pk.TeraType;
+        Teratypepicker.SelectedIndex = (int) pk.TeraTypeOverride == 0x13 ? (int)pk.TeraTypeOriginal : (int)pk.TeraTypeOverride;
+        MainTeratypepicker.SelectedIndex = (int)pk.TeraTypeOriginal;
         var str = GameInfo.Strings;
-        var forms = FormConverter.GetFormList(pk.Species, str.types, str.forms, GameInfo.GenderSymbolUnicode, EntityContext.Gen8);
-        formpicker.ItemsSource = forms;
+        var forms = FormConverter.GetFormList(pk.Species, str.types, str.forms, GameInfo.GenderSymbolUnicode, EntityContext.Gen9);
+        if(forms.Count() != 0) 
+        { 
+            formpicker.ItemsSource = forms;
         formpicker.SelectedIndex = pk.Form;
+        }
+        if(abilitypicker.Items.Count() != 0)
+            abilitypicker.Items.Clear();
+        for(int i = 0; i<3; i++)
+        {
+            var abili = pk.PersonalInfo.GetAbilityAtIndex(i);
+            abilitypicker.Items.Add($"{(Ability)abili}");
+            
+        }
+
+
+
+       
+        Friendshipdisplay.Text = $"{pk.CurrentFriendship}";
+        Heightdisplay.Text = $"{pk.HeightScalar}";
+        Weightdisplay.Text =$"{pk.WeightScalar}";
+        scaledisplay.Text = $"{pk.Scale}";
+        genderdisplay.SelectedIndex = pk.Gender;
+        
+
+
+
+
 
     }
-
+    private static int GetSafeIndex(Picker cb, int index) => Math.Max(0, Math.Min(cb.Items.Count - 1, index));
     public async void pk9saver_Clicked(object sender, EventArgs e)
     {
-        #if ANDROID
-            await File.WriteAllBytesAsync($"/storage/emulated/0/Documents/{(Species)pk.Species}.pk9", pk.pkdata);
-        #endif
+        
+            await File.WriteAllBytesAsync($"/storage/emulated/0/Documents/{(Species)pk.Species}.pk9", pk.DecryptedPartyData);
+        
     }
 
     private void specieschanger(object sender, EventArgs e) => pk.Species = (ushort)specieslabel.SelectedIndex;
@@ -56,17 +96,119 @@ public partial class MainPage : ContentPage
     private void applynickname(object sender, TextChangedEventArgs e)
     {
         pk.SetNickname(pk.Nickname);
-        nickname.Text = pk.Nickname;
+        
     }
 
     private void turnshiny(object sender, EventArgs e)
     {
         if (!pk.IsShiny)
+        {
             pk.SetIsShiny(true);
-        else
+            shinybutton.Text = "★";
+        }
+        else 
+        {
             pk.SetIsShiny(false);
+            shinybutton.Text = "☆";
+        }
+
         displaypid.Text = $"{pk.PID:X}";
         
+    }
+
+    private void applyexp(object sender, TextChangedEventArgs e) => pk.EXP = uint.Parse(exp.Text);
+
+    private void applynature(object sender, EventArgs e) => pk.Nature = naturepicker.SelectedIndex;
+
+    private void applytera(object sender, EventArgs e) => pk.TeraTypeOverride = (MoveType)Teratypepicker.SelectedIndex;
+
+    private void applymaintera(object sender, EventArgs e) => pk.TeraTypeOriginal = (MoveType)MainTeratypepicker.SelectedIndex;
+
+    private void applyform(object sender, EventArgs e) => pk.Form = (byte)formpicker.SelectedIndex;
+
+    private void applyhelditem(object sender, EventArgs e) => pk.ApplyHeldItem(helditempicker.SelectedIndex, EntityContext.Gen8);
+
+    private void applyability(object sender, EventArgs e) => pk.SetAbility((int)(Ability)abilitypicker.SelectedItem);
+
+    private void botbaseconnect(object sender, EventArgs e)
+    {
+        if (!SwitchConnection.Connected)
+        {
+            SwitchConnection.Connect(IP.Text,6000);
+            connect.Text = "disconnect";
+        }
+        else
+        {
+            SwitchConnection.Disconnect(true);
+            connect.Text = "connect";
+        }
+    }
+
+    private async void inject(object sender, EventArgs e)
+    {
+        IEnumerable<long> jumps = new long[] { 0x42FD510, 0xA90, 0x9B0, 0x0 };
+        var off = await botBase.PointerRelative(jumps);
+        await botBase.WriteBytesAsync(pk.EncryptedPartyData, (uint)off);
+    }
+
+    private void changelevel(object sender, TextChangedEventArgs e)
+    {
+        pk.CurrentLevel = int.Parse(leveldisplay.Text);
+        exp.Text = $"{Experience.GetEXP(pk.CurrentLevel, pk.PersonalInfo.EXPGrowth)}";
+    }
+
+    private void applyfriendship(object sender, TextChangedEventArgs e) => pk.CurrentFriendship = int.Parse(Friendshipdisplay.Text);
+
+    private void applyheight(object sender, TextChangedEventArgs e) => pk.HeightScalar = (byte)int.Parse(Heightdisplay.Text);
+
+    private void applyweight(object sender, TextChangedEventArgs e) => pk.WeightScalar = (byte)int.Parse(Weightdisplay.Text);
+
+    private void applyscale(object sender, TextChangedEventArgs e) => pk.Scale = (byte)int.Parse(scaledisplay.Text);
+
+    private void applygender(object sender, EventArgs e) => pk.SetGender(genderdisplay.SelectedIndex);
+
+    private async void read(object sender, EventArgs e)
+    {
+        IEnumerable<long> jumps = new long[] { 0x42FD510, 0xA90, 0x9B0, 0x0 };
+        var off = await botBase.PointerRelative(jumps);
+        var bytes = await botBase.ReadBytesAsync((uint)off, 344);
+        pk = new(bytes);
+
+        if (pk.IsShiny)
+            shinybutton.Text = "★";
+        specieslabel.SelectedIndex = pk.Species;
+        displaypid.Text = $"{pk.PID:X}";
+        nickname.Text = pk.Nickname;
+        exp.Text = $"{pk.EXP}";
+        leveldisplay.Text = $"{Experience.GetLevel(pk.EXP, pk.PersonalInfo.EXPGrowth)}";
+        naturepicker.SelectedIndex = pk.Nature;
+        Teratypepicker.SelectedIndex = (int)pk.TeraTypeOverride == 0x13 ? (int)pk.TeraTypeOriginal : (int)pk.TeraTypeOverride;
+        MainTeratypepicker.SelectedIndex = (int)pk.TeraTypeOriginal;
+        var str = GameInfo.Strings;
+        var forms = FormConverter.GetFormList(pk.Species, str.types, str.forms, GameInfo.GenderSymbolUnicode, EntityContext.Gen9);
+        if (forms.Count() != 0)
+        {
+            formpicker.ItemsSource = forms;
+            formpicker.SelectedIndex = pk.Form;
+        }
+        if (abilitypicker.Items.Count() != 0)
+            abilitypicker.Items.Clear();
+        for (int i = 0; i < 3; i++)
+        {
+            var abili = pk.PersonalInfo.GetAbilityAtIndex(i);
+            abilitypicker.Items.Add($"{(Ability)abili}");
+
+        }
+
+
+
+        
+        Friendshipdisplay.Text = $"{pk.CurrentFriendship}";
+        Heightdisplay.Text = $"{pk.HeightScalar}";
+        Weightdisplay.Text = $"{pk.WeightScalar}";
+        scaledisplay.Text = $"{pk.Scale}";
+        genderdisplay.SelectedIndex = pk.Gender;
+
     }
 }
 
