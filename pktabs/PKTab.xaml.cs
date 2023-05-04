aec474b10221dc12b7e252c895fc9c4f394e17ac
Santacrab2 <skaradiodj@gmail.com>
2023-05-04 14:18:20 -0400

    - update version for release

--- pktabs/PKTab.xaml.cs
+++ pktabs/PKTab.xaml.cs
@@ -12,7 +12,7 @@ namespace PKHeXMAUI;
 
 public partial class MainPage : ContentPage
 {
-    public static string Version = "v23.5.3";
+    public static string Version = "v23.5.4";
     public bool SkipTextChange = false;
     public int[] NoFormSpriteSpecies = new[] { 664, 665, 744, 982, 855, 854, 869 };
     public MainPage()
