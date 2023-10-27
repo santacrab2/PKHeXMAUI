b8d7efa73cee21d3fd956ef9e1af0d995a3b3331
Santacrab2 <skaradiodj@gmail.com>
2023-10-27 08:04:05 -0400

    more livehex functions

--- LiveHex/LiveHex.xaml.cs
+++ LiveHex/LiveHex.xaml.cs
@@ -5,14 +5,19 @@ using static MainPage;
 
 public partial class LiveHex : ContentPage
 {
-	public LiveHex()
+    public static bool SkipTextChanges = false;
+    public LiveHex()
 	{
 		InitializeComponent();
 		Port.Text = sav.Generation > 7 ? "6000" : "8000";
 		var validvers = RamOffsets.GetValidVersions(sav);
 		var com = RamOffsets.GetCommunicator(sav, InjectorCommunicationType.SocketNetwork);
 		Remote = new PokeSysBotMini(validvers[0], com, false);
-	}
+        var ip = Preferences.Default.Get("IP", "192.168.1.1");
+        SkipTextChanges = true;
+        IP.Text = ip;
+        SkipTextChanges = false;
+    }
 	public static PokeSysBotMini Remote;
 
     private async void botbaseconnect(object sender, EventArgs e)
@@ -27,4 +32,12 @@ public partial class LiveHex : ContentPage
 		Remote.com.Port = int.Parse(Port.Text);
 		Remote.com.Connect();
     }
+
+ 
+    private void SaveUserIP(object sender, TextChangedEventArgs e)
+    {
+        if (!SkipTextChanges && IP.Text.Length > 0)
+            Preferences.Set("IP", IP.Text);
+    }
+    
 }
