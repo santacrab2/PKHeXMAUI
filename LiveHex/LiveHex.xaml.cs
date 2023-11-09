24fe3d46034628fd4487249e7217e319a686e2bb
Santacrab2 <skaradiodj@gmail.com>
2023-11-09 11:53:07 -0500

    - read and write current box

--- LiveHex/LiveHex.xaml.cs
+++ LiveHex/LiveHex.xaml.cs
@@ -16,10 +16,13 @@ public partial class LiveHex : ContentPage
         var ip = Preferences.Default.Get("IP", "192.168.1.1");
         SkipTextChanges = true;
         IP.Text = ip;
+        CB_InjectinSlot.IsChecked = InjectinSlot;
+        CB_ReadChangeBox.IsChecked = ReadonChangeBox;
         SkipTextChanges = false;
     }
 	public static PokeSysBotMini Remote;
-
+    public static bool ReadonChangeBox = Preferences.Get("ReadonChangeBox", true);
+    public static bool InjectinSlot = Preferences.Get("InjectinSlot", true);
     private async void botbaseconnect(object sender, EventArgs e)
     {
         IEnumerable<ConnectionProfile> profiles = Connectivity.Current.ConnectionProfiles;
@@ -69,5 +72,39 @@ public partial class LiveHex : ContentPage
             return;
         }
         pk = EntityFormat.GetFromBytes(Remote.ReadSlot(box-1, slot-1));
+    }
+
+    private void B_ReadFromOff_Click(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void CB_ReadChangeBox_Check(object sender, CheckedChangedEventArgs e)
+    {
+        if(!SkipTextChanges)
+            Preferences.Set("ReadonChangeBox", CB_ReadChangeBox.IsChecked);
+    }
+
+    private void CB_InjectinSlot_check(object sender, CheckedChangedEventArgs e)
+    {
+        if(!SkipTextChanges)
+            Preferences.Set("InjectinSlot", CB_InjectinSlot.IsChecked);
+    }
+
+    private void B_ReadCurrentBox_Click(object sender, EventArgs e)
+    {
+        var box = BoxTab.CurrentBox;
+        var len =
+               sav.BoxSlotCount
+               * (RamOffsets.GetSlotSize(Remote.Version) + RamOffsets.GetGapSize(Remote.Version));
+        var data = Remote.ReadBox(box, len).AsSpan();
+        sav.SetBoxBinary(data, box);
+        
+    }
+
+    private void B_WriteCurrentBox_Click(object sender, EventArgs e)
+    {
+        var boxData = sav.GetBoxBinary(BoxTab.CurrentBox);
+        Remote.SendBox(boxData, BoxTab.CurrentBox);
     }
 }
