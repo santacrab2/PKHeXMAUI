d202622d491c3a4b4784aa788e9fdab001ec2d24
Santacrab2 <skaradiodj@gmail.com>
2023-11-12 13:28:15 -0500

    - bump version

--- pktabs/PKTab.xaml.cs
+++ pktabs/PKTab.xaml.cs
@@ -12,7 +12,7 @@ namespace PKHeXMAUI;
 
 public partial class MainPage : ContentPage
 {
-    public static string Version = "v23.11.11";
+    public static string Version = "v23.11.12";
     public bool SkipTextChange = false;
     public static int[] NoFormSpriteSpecies = new[] { 664, 665, 744, 982, 855, 854, 869,892,1012,1013 };
     public bool FirstLoad = true;
